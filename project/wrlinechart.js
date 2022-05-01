132d43af723b7aaa970d445bf3e091d64d01ce8d
igentle292 <31328937+igentle292@users.noreply.github.com>
2022-05-01 16:03:16 -0700

    added simple line chart

---
+++ project/wrlinechart.js
@@ -0,0 +1,100 @@
+let parseDate = d3.timeParse("%m/%d/%Y");
+let parseTime = d3.timeParse("%H:%M:%S");
+
+function converter(d){
+    d.Date = parseDate(d.Date);
+    d.Time = parseTime(d.Time);
+    return d;
+}
+
+function main(){
+    let margin = 150;
+    let width = 1000 - margin;
+    let height = 600 - margin;
+
+    let svg = d3.select("#line").append("svg")
+        .attr("width", width + margin)
+        .attr("height", height + margin);
+
+    let container_g = svg.append("g")
+        .attr("transform",
+            "translate(" + 100 + ", " + 0 + ")");
+
+    let xScale = d3.scaleTime().range([0, width]);
+    let yScale = d3.scaleTime().range([height, 0]);
+
+    d3.csv("SM64 World Record History.csv", converter).then(data => {
+        container_g.append("text")
+            .attr("y", 50)
+            .attr("x", 250)
+            .attr("stroke", "black")
+            .attr("font-family", "sans-serif")
+            .text("SM 64 World Record History, 2006-2022");
+
+        xScale.domain([d3.min(data.map(function(d){return d.Date;})), parseDate("9/1/2022")]);
+
+        yScale.domain([parseTime("1:30:00"), parseTime("2:15:00")]);
+
+        const newData = data.map(function(d){
+            return [xScale(d.Date), yScale(d.Time)];
+        });
+
+        let line = d3.line();
+
+        container_g.append("g")
+            .attr("transform", "translate(20, 100)")
+            .append("path")
+            .attr("d", line(newData))
+            .attr("stroke", "green")
+            .attr("stroke-width", 2)
+            .attr("fill-opacity", 0);
+
+        container_g.append("g")
+            .attr("transform", "translate(20, 100)")
+            .selectAll("circle")
+            .data(newData)
+            .enter()
+            .append("circle")
+            .style("cx", function(d){
+                return d[0];
+            })
+            .style("cy", function(d){
+                return d[1];
+            })
+            .style("r", 4)
+            .style("fill", "lightgrey")
+            .style("stroke", "green");
+
+
+        //Code from lecture
+        container_g.append("g")
+            .attr("transform", "translate(0, 550)")
+            .call(d3.axisBottom(xScale))
+            .append("text")
+            .attr("y", 45)
+            .attr("x", 500)
+            .attr("stroke", "black")
+            .attr("fill", "black")
+            .attr("font-size", 14)
+            .text("Date");
+
+        // Display the Y-axis
+        container_g.append("g")
+            .attr("transform", "translate(0, 100)")
+            .call(d3.axisLeft(yScale).tickFormat(function(d) {
+                let axisString = d.toLocaleTimeString();
+                return axisString.substring(0, axisString.length - 3);
+            }).ticks(10))
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -5)
+            .attr("x", -150)
+            .attr("dy", "-5.1em")
+            .attr("stroke", "black")
+            .attr("fill", "black")
+            .attr("font-size", 14)
+            .text("Time taken to complete(H:MM:SS)");
+    })
+}
+
+main();
